22f727c52219a63461b32c19321ccc10f87dbb6d
Jackson Collier <jcolli44@vols.utk.edu>
2016-04-04 11:35:09 -0400

    Further into Day 7 Solution
    Still have a lot of work to do to get this
    to work right.

--- day07/day07.py
+++ day07/day07.py
@@ -1,61 +1,122 @@
 #!/usr/bin/env python3
 
 from ctypes import *
+import re
 
 class Circuit:
     def __init__(self, filename):
         self.Operations = ["AND", "OR", "LSHIFT", "RSHIFT",
                         "NOT"]
+        self.Commands = []
         self.wires = {}
  
         with open(filename, "r") as fin:
             instructions = fin.readlines()
             for command in instructions:
-                self.parse_operation(command)
-
-    def parse_operation(self, command):
-        print("Command: %s" % command)
-        print("=================")
-        components = command.split("->")
-        command_operation = "" 
-        for operation in self.Operations:
-            if operation in command:
-                command_operation = operation
-                if command_operation == "NOT":
-                    operands = components[0].replace("NOT", "")
-                    operands.strip(" ")
-                else:
-                    operands = components[0].split(command_operation)
-        
-        if command_operation == "":
-            command_operation = "STORE"
-            operands = components[0].rstrip()
-
-        print("Command Operation: %s\n" % command_operation)
-        print(operands)
-        print(components[1])
-        print("\n")
-
-    def perform_operation(command_operation, operands, store_result):
-        if command_operation != "STORE":
-            if command_operation == "AND":
-                first_operand = c_ushort(self.wires[operands[0].strip(' ')])
-                second_operand = c_ushort(self.wires[operands[1].strip(' ')])
-                result = c_ushort(first_operand & second_operand)
-            elif command_operation == "OR":
-                first_operand = c_ushort(self.wires[operands[0].strip(' ')])
-                second_operand = c_ushort(self.wires[operands[1].strip(' ')])
-                result = c_ushort(first_operand | second_operand)
-            elif command_operation == "LSHIFT":
-                first_operand = c_ushort(self.wires[operands[0].strip(' ')])
-                second_operand = int(operands[1].strip(' '))
-                result = c_ushort(first_operand << second_operand)
-            elif command_operation == "RSHIFT":
-                first_operand = c_ushort(self.wires[operands[0].strip(' ')])
-                second_operand = int(operands[1].strip(' '))
-                result = c_ushort(first_operand >> second_operand)
-            elif command_operation == "NOT":
-                first_operand = c_ushort(self.operands[1].strip(' '))
-                result = c_ushort(~first_operand)
+                operation = ""
+                for op in self.Operations:
+                    if op in command:
+                        operation = op
+                if operation == "":
+                    operation = "STORE"
+                command = Command(command.replace("\n", ""), operation.replace(" ", ""), self.wires)
+                self.Commands.append(command)
     
-c = Circuit("test001.txt")
+    def analyze(self):
+        while (len(self.Commands)):
+            for command in self.Commands:
+             command.check_state()
+             if command.can_execute:
+                command.execute()
+                self.Commands.remove(command)
+       
+    def print_commands(self):
+        for command in self.Commands:
+            command.print_state() 
+                 
+class Command:
+    def __init__(self, command, operation, wires):
+        self.command = command
+        self.operation = operation
+        self.wires = wires         
+        self.components = self.command.split("->")
+        self.num_operands = 0
+        self.operands = []
+        self.store_result_location = ""
+        self.can_execute = False
+        
+        if self.operation == "NOT":
+            operand = Operand(self.components[0].replace("NOT ", ""), self.wires)
+            self.operands.append(operand)
+            self.num_operands = 1
+        elif self.operation == "STORE":
+            operand = Operand(self.components[0].replace(" ", ""), self.wires)
+            self.operands.append(operand)
+            self.num_operands = 1
+        else: 
+            self.num_operands = 2
+            left_side_operands = self.components[0].split(self.operation)
+            operand = Operand(left_side_operands[0].replace(" ", ""), self.wires)
+            self.operands.append(operand)
+            operand = Operand(left_side_operands[1].replace(" ", ""), self.wires)
+            self.operands.append(operand)
+
+        self.store_result_location = self.components[1].replace(" ", "") 
+    
+    def execute(self):
+        if self.operation == "STORE":
+            self.wires[self.store_result_location] = c_ushort(operands[0].numeric_val)
+        elif self.operation == "AND":
+            self.wires[self.store_result_location] = c_ushort(operands[0].numeric_val & operands[1].numeric_val)
+        elif self.operation == "OR":
+            self.wires[self.store_result_location] = c_ushort(operands[0].numeric_val | operands[1].numeric_val)
+        elif self.operation == "RSHIFT":
+            self.wires[self.store_result_location] = c_ushort(operands[0].numeric_val >> operands[1].numeric_val)
+        elif self.operation == "LSHIFT":
+            self.wires[self.store_result_location] = c_ushort(operands[0].numeric_val << operands[1].numeric_val)
+        elif self.operation == "NOT": 
+            self.wires[self.store_result_lovation] = c_ushort(~operands[0].numeric_val)
+
+    def check_state(self):
+        for operand in self.operands:
+            if not operand.check_numeric_value():
+                return False
+        
+        return True
+                
+    def print_state(self):
+        print("===========================================")
+        print("Operation: %s --- Num Operands: %d --- Executable: %r" % (self.operation, self.num_operands, self.check_state()))
+        print("    Operands: ")
+        for op in self.operands:
+            print("        Operand: %s --- Type: %s" % (op.val, op.val_type))
+        print("Store Result in Location: %s" % (self.store_result_location))
+        print()
+                     
+class Operand: 
+    def __init__(self, operand, wires):
+        self.val = operand
+        self.has_numeric_val = False
+        self.numeric_val = -1
+        self.val_type = ""
+        self.wires = wires
+
+        if re.match("[a-zA-Z]", self.val) != None:
+            self.val_type = "GATE_NAME"
+        else:
+            self.val_type = "NUMERIC_LITERAL"
+            self.has_numeric_val = True
+            self.numeric_val = int(self.val)
+    
+    def check_numeric_value(self):
+        if self.has_numeric_val:
+            return True
+        if self.val in self.wires:
+            self.numeric_val = int(self.wires[self.val])
+        
+        return False
+
+circuit = Circuit("test001.txt")
+circuit.print_commands()
+circuit.analyze()
+print(circuit.wires)
