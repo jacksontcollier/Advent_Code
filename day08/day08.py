1fe805c22d64cce85af15998513cd53ea94b59ab
Jackson Collier <jcolli44@vols.utk.edu>
2016-04-12 00:48:09 -0400

    Finish solutions for day 08
    Probably should refactor later.

--- day08/day08.py
+++ day08/day08.py
@@ -12,24 +12,31 @@ class SantasList:
         
         with open(self.filename, "r") as fin:
             for line in fin.readlines():
-                line = line.rstrip('\n')
-                print(line)
-                total = len(line) 
-                self.literal_total += total
-                print(line) 
-                num_escaped_backslash = len(re.findall(r"\\\\", line))
-                num_escaped_quote = len(re.findall(r"\\\"", line))
-                num_escaped_hexnotation = len(re.findall(r"\\x[0-9a-fA-f]{2}", line))
-                if line[0] == "\"": total -= 1
-                if line[-1] == "\"": total -=1
-                total -= num_escaped_backslash
-                total -= num_escaped_quote
-                total -= (3 * num_escaped_hexnotation)
-                self.inmemory_total += total
+                line = line.rstrip()
+                self.literal_total += len(line)
+                line = line[1: len(line) - 1]
+                for match in re.findall(r"\\\\", line): line = line.replace(match, "a")
+                for match in re.findall(r"\\\"", line): line = line.replace(match, "a")
+                for match in re.findall(r"\\x[0-9a-f]{2}", line): line = line.replace(match, "a")
+                self.inmemory_total += len(line)
 
         print(self.literal_total - self.inmemory_total)
 
-        
+    def expand(self):
+        self.original_total = 0
+        self.expanded_total = 0
 
-list = SantasList("test001.txt") 
-list.count_differences()
+        with open(self.filename, "r") as fin:
+            for line in fin.readlines():
+                line = line.rstrip()
+                self.original_total += len(line)
+                self.expanded_total += len(line) + 2
+                for c in line:
+                    if (c == "\"") or (c == "\\"):
+                        self.expanded_total += 1
+
+        print(self.expanded_total - self.original_total)
+
+annoying_list = SantasList("input_day08.txt") 
+annoying_list.count_differences()
+annoying_list.expand()
