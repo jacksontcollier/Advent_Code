c769fdecef9756b30221d957860f736058161a51
Jackson Collier <jcolli44@vols.utk.edu>
2016-05-08 23:09:01 -0400

    Finish Advent of Code day 16 solution
    Remove compare method from Aunt_Sue and make a stand alone function
    
    Add a new compare retro function that embodies comparisons dictated by day 16 part two
    
    Pass comparison function as a parammeter to find giftsending_aunt_sue

--- day16/day16.py
+++ day16/day16.py
@@ -20,22 +20,33 @@ class Aunt_Sue:
         for i in range(len(props)):
             self.properties[props[i]] = stats[i]
 
-    def could_be(self, different_sue):
-        for prop in different_sue.properties.keys():
-            if prop not in self.properties:
-                return False
-            
-            if different_sue.properties[prop] != self.properties[prop]:
-                return False
 
-        return True
-
-
-def find_giftsending_aunt_sue(ticker_tape_aunt_sue, data_file):
+def find_giftsending_aunt_sue(ticker_tape_aunt_sue, data_file, could_be_compare):
     with open(data_file) as fin:
         for line in fin.readlines():
             candidate = Aunt_Sue(line.rstrip())
-            if ticker_tape_aunt_sue.could_be(candidate):
+            if could_be_compare(ticker_tape_aunt_sue, candidate):
                 return candidate.number
 
     return 0
+
+def could_be(ticker_tape_sue, different_sue):
+    for prop in different_sue.properties.keys():
+        if different_sue.properties[prop] != ticker_tape_sue.properties[prop]:
+            return False
+
+    return True
+
+def could_be_retro(ticker_tape_sue, different_sue):
+    for prop in different_sue.properties.keys():
+        if prop == "cats" or prop == "trees":
+            if different_sue.properties[prop] <= ticker_tape_sue.properties[prop]:
+                return False
+        elif prop == "pomeranians" or prop == "goldfish":
+            if different_sue.properties[prop] >= ticker_tape_sue.properties[prop]:
+                return False
+        else:
+            if different_sue.properties[prop] != ticker_tape_sue.properties[prop]:
+                return False
+    
+    return True
--- day16/get_answer.py
+++ day16/get_answer.py
@@ -2,6 +2,8 @@
 
 from day16 import Aunt_Sue
 from day16 import find_giftsending_aunt_sue
+from day16 import could_be
+from day16 import could_be_retro
 
 ticker_tape_aunt_sue = None
 
@@ -9,4 +11,7 @@ with open('ticker_tape_aunt_sue.txt') as fin:
     for line in fin.readlines():
         ticker_tape_aunt_sue = Aunt_Sue(line.rstrip())
 
-print("Answer 1: %d" % find_giftsending_aunt_sue(ticker_tape_aunt_sue, 'day16_input.txt'))
+print("Answer 1: %d" % find_giftsending_aunt_sue(
+        ticker_tape_aunt_sue, 'day16_input.txt', could_be))
+print("Answer 2: %d" % find_giftsending_aunt_sue(
+        ticker_tape_aunt_sue, 'day16_input.txt', could_be_retro))
