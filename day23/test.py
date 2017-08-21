111398b04940a51951b3f3e23ab5613ae138220e
Jackson Collier <jacksontcollier@gmail.com>
2017-08-20 22:25:39 -0400

    Add new unit test for instruction to execution method mapping

--- day23/test.py
+++ day23/test.py
@@ -24,5 +24,43 @@ class MachineExecutesProgramCorrectly(unittest.TestCase):
             machine.execute_program(instructions)
             self.assertEqual(2, machine.get_register("a"))
 
+class ExecuteInstructionCallsCorrectInstructionHandler(unittest.TestCase):
+    def test_one(self):
+        with mock.patch.object(Machine, "execute_hlf_instruction") as mock_method:
+            vm = Machine()
+            vm.execute_instruction("hlf a")
+
+        mock_method.assert_called_with("a")
+
+        with mock.patch.object(Machine, "execute_tpl_instruction") as mock_method:
+            vm = Machine()
+            vm.execute_instruction("tpl a")
+
+        mock_method.assert_called_with("a")
+
+        with mock.patch.object(Machine, "execute_inc_instruction") as mock_method:
+            vm = Machine()
+            vm.execute_instruction("inc a")
+
+        mock_method.assert_called_with("a")
+
+        with mock.patch.object(Machine, "execute_jmp_instruction") as mock_method:
+            vm = Machine()
+            vm.execute_instruction("jmp +23")
+
+        mock_method.assert_called_with("+23")
+
+        with mock.patch.object(Machine, "execute_jie_instruction") as mock_method:
+            vm = Machine()
+            vm.execute_instruction("jie a +4")
+
+        mock_method.assert_called_with("a", "+4")
+
+        with mock.patch.object(Machine, "execute_jio_instruction") as mock_method:
+            vm = Machine()
+            vm.execute_instruction("jio a +19")
+
+        mock_method.assert_called_with("a", "+19")
+
 if __name__ == "__main__":
     unittest.main()
