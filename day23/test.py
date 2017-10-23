fc73f7942acc7587cf0db8afdefcd7cf8d457853
Jackson Collier <jacksontcollier@gmail.com>
2017-10-22 21:42:45 -0400

    Use module name to access module attributes

--- day23/test.py
+++ day23/test.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
 
 import unittest
-import unittest.mock as mock
-from machine import read_instructions_from_file
-from machine import Machine
+
+import machine
 
 class ReadsInstructionsFromFileCorrectly(unittest.TestCase):
     def test_one(self):
         test_data = "inc a\njio a, +2\ntpl a\ninc a\n"
         expected = ["inc a", "jio a +2", "tpl a", "inc a"]
 
-        with mock.patch("builtins.open", mock.mock_open(read_data=test_data)):
-            actual = read_instructions_from_file("test")
+        with unittest.mock.patch("builtins.open",
+                                 unitest.mock.mock_open(read_data=test_data)):
+            actual = machine.read_instructions_from_file("test")
             self.assertEqual(expected, actual)
 
 class MachineExecutesProgramCorrectly(unittest.TestCase):
     def test_one(self):
         test_data = "inc a\njio a, +2\ntpl a\ninc a\n"
 
-        with mock.patch("builtins.open", mock.mock_open(read_data=test_data)):
-            instructions = read_instructions_from_file("test")
-            machine = Machine()
-            machine.execute_program(instructions)
-            self.assertEqual(2, machine.get_register("a"))
+        with unittest.mock.patch("builtins.open",
+                                 unittest.mock.mock_open(read_data=test_data)):
+            instructions = machine.read_instructions_from_file("test")
+            virtual_machine = machine.Machine()
+            virtual_machine.execute_program(instructions)
+            self.assertEqual(2, virtual_machine.get_register("a"))
 
 class ExecuteInstructionCallsCorrectInstructionHandler(unittest.TestCase):
     def test_one(self):
-        with mock.patch.object(Machine, "execute_hlf_instruction") as mock_method:
+        with mock.patch.object(machine.Machine, "execute_hlf_instruction") as mock_method:
             vm = Machine()
             vm.execute_instruction("hlf a")
 
